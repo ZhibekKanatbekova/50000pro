7159a681e0553dfe81e1540a915180ab99a65828
Zhibek <jiko633@gmail.com>
2021-04-07 15:30:17 +0400

    app.js has been added

---
+++ app.js
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+const product_img = Array.from(document.querySelectorAll(".img1"))[0].src;
+const productn = Array.from(document.querySelectorAll(".title"))[0].innerHTML;
+const price = Array.from(document.querySelectorAll(".price1"))[0].innerHTML;
+console.log(product_img);
+console.log(productn);
+console.log(price)
