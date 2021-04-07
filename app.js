d7887b3f875677313ed3914340502e8a52739ab7
Zhibek <jiko633@gmail.com>
2021-04-07 19:52:17 +0400

    31lesson added

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /*jshint esversion: 6 */
 
-
 // const chart = Array.from(document.querySelectorAll(".fa-shopping-bag"));
 // function myFunction (){
 //   let namePic = Array.from(this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".title"));
@@ -17,9 +16,20 @@ function rad(a){
 return 2 * 3.14159 *a;
 }
 alert(rad(num));
+
+
 let fnum = parseInt(prompt("First number please"));
-let snum = parseInt(prompt("Second number please"));
-function min (x, y ){
-  return Math.min(x,y)
+let snum = parseInt(prompt("Power number please"));
+function pove(x, y ){
+  return Math.pow(x,y);
 }
-alert(min(fnum,snum))
+alert(pove(fnum, snum));
+
+
+
+let wid = parseInt(prompt("give me wid number"));
+let len = parseInt(prompt("give me length number"));
+function triagle(w,l) {
+  return w*l;
+}
+alert(triagle(wid,len));
